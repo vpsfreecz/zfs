cf6f1e8f07ffc5ca9efc249771c46db472bff2d8
Jakub Skokan <jakub.skokan@havefun.cz>
2018-05-14 17:22:39 +0200

    [vpsAdminOS] Configurable UID/GID maps
    This patch introduces two new properties: uidmap and gidmap.
    These properties are used to map user and group IDs to a different
    namespace. The mapping occurs only in memory and what is presented to VFS.
    On disk, the IDs are not shifted, which means the map can be changed
    at will without chowning all the files within the dataset.
    
    This feature is useful for unprivileged LXC containers that are using
    UID/GID mapping. Create the container _without_ any idmap, then
    configure the mapping in LXC and set appropriate uidmap and gidmap
    on the container's dataset.
    
    For example:
    
      zfs set uidmap="0:100000:65536" tank/test
      zfs set gidmap="0:100000:65536" tank/test
    
    The commands above will make it so that UID/GID 0-65535 is mapped to
    100000-165535. To use this dataset in a user namespace, you could do
    something like `newuidmap <pid> 0 100000 65536` or in LXC
    `lxc.idmap = u 0 100000 65536` and the same for group IDs.
    
    ZFS checks that the map is syntactically correct and can process up to
    10 map entries separated by commas. However, it does not check whether
    the mapping makes sense logically, or if there aren't some overlaps. Map
    entries are searched for sequentially, first matching entry wins.

--- include/os/linux/zfs/sys/zfs_vfsops_os.h
+++ include/os/linux/zfs/sys/zfs_vfsops_os.h
@@ -37,6 +37,7 @@
 #include <sys/dsl_dataset.h>
 #include <sys/zfs_ioctl.h>
 #include <sys/objlist.h>
+#include <sys/zfs_ugid_map.h>
 
 #ifdef	__cplusplus
 extern "C" {
@@ -134,6 +135,8 @@ struct zfsvfs {
 	avl_tree_t	*z_hold_trees;	/* znode hold trees */
 	kmutex_t	*z_hold_locks;	/* znode hold locks */
 	taskqid_t	z_drain_task;	/* task id for the unlink drain task */
+	struct zfs_ugid_map    *z_uid_map;
+	struct zfs_ugid_map    *z_gid_map;
 };
 
 #define	ZFS_TEARDOWN_INIT(zfsvfs)		\
