5fa4aacb9c64ffef7c3a184349966da5a8df8b35
Pavel Snajdr <snajpa@snajpa.net>
2024-10-17 21:43:30 +0200

    wip
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- module/os/linux/zfs/zfs_znode_os.c
+++ module/os/linux/zfs/zfs_znode_os.c
@@ -1090,10 +1090,10 @@ zfs_zget(zfsvfs_t *zfsvfs, uint64_t obj_num, znode_t **zpp)
 
 		ASSERT3P(zp, !=, NULL);
 		if (igrab(ZTOI(zp)) == NULL) {
-			dump_stack();
+			//dump_stack();
 			sa_buf_rele(db, NULL);
 			zfs_znode_hold_exit(zfsvfs, zh);
-			return (SET_ERROR(EIO));
+			return (SET_ERROR(EAGAIN));
 		}
 		*zpp = zp;
 
