f21a5df85eddf01d8c4721586d877fa2af9f8391
Pavel Snajdr <snajpa@snajpa.net>
2025-05-15 15:21:05 +0200

    Linux: use d_instantiate_new as per Documentation
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- module/os/linux/zfs/zpl_inode.c
+++ module/os/linux/zfs/zpl_inode.c
@@ -204,8 +204,7 @@ zpl_create(struct inode *dir, struct dentry *dentry, umode_t mode, bool flag)
 			(void) zfs_remove(ITOZ(dir), dname(dentry), cr, 0);
 		} else {
 			VERIFY0(insert_inode_locked(ZTOI(zp)));
-			d_instantiate(dentry, ZTOI(zp));
-			unlock_new_inode(ZTOI(zp));
+			d_instantiate_new(dentry, ZTOI(zp));
 		}
 	}
 
@@ -417,8 +416,7 @@ zpl_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
 			(void) zfs_rmdir(ITOZ(dir), dname(dentry), NULL, cr, 0);
 		} else {
 			VERIFY0(insert_inode_locked(ZTOI(zp)));
-			d_instantiate(dentry, ZTOI(zp));
-			unlock_new_inode(ZTOI(zp));
+			d_instantiate_new(dentry, ZTOI(zp));
 		}
 	}
 
