a919601118ac89efc0ce011a9fddc0083007fb5f
Alexander Motin <mav@FreeBSD.org>
2025-05-22 10:47:43 -0400

    Fix null dereference in spa_vdev_remove_cancel_sync()
    We don't really need to access space map to know where the metaslab
    ends, while msp->ms_sm might be NULL.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Rob Norris <robn@despairlabs.com>
    Reviewed by: Igor Kozhukhov <ikozhukhov@gmail.com>
    Signed-off-by:	Alexander Motin <mav@FreeBSD.org>
    Sponsored by:	iXsystems, Inc.
    
    Upstream: 5c30b24381644a9d1b83d51e813e5e7efba23bc6
    
    Ported-by: codex-5.3-xhigh
    Porting-notes:
    - Conflict with earlier device-removal cancel backport: applied upstream fix by using metaslab start+size (ms_end) when clearing segments, avoiding any ms_sm dereference.

--- module/zfs/vdev_removal.c
+++ module/zfs/vdev_removal.c
@@ -1937,16 +1937,9 @@ spa_vdev_remove_cancel_sync(void *arg, dmu_tx_t *tx)
 			 * because we have not allocated mappings for it yet.
 			 */
 			uint64_t syncd = vdev_indirect_mapping_max_offset(vim);
-			uint64_t seg_end;
-			if (msp->ms_sm != NULL) {
-				seg_end = msp->ms_sm->sm_start +
-				    msp->ms_sm->sm_size;
-			} else {
-				seg_end = msp->ms_start + msp->ms_size;
-			}
-			if (seg_end > syncd)
-				zfs_range_tree_clear(segs, syncd,
-				    seg_end - syncd);
+			uint64_t ms_end = msp->ms_start + msp->ms_size;
+			if (ms_end > syncd)
+				zfs_range_tree_clear(segs, syncd, ms_end - syncd);
 
 		zfs_range_tree_vacate(segs, free_mapped_segment_cb, vd);
 	}
