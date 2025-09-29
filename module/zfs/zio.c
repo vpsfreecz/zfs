3a1b732d7bd4b45e087402b1585b70cf3b480fe1
Igor Ostapenko <igor.ostapenko@klarasystems.com>
2025-09-29 10:58:52 +0000

    zio: use spa_config_enter_mmp for SCL_ZIO priority
    Upstream: cb3c18a9a93dcff9b18f48b72aafcc06342c63e3
    
    Ported-by: codex-5.3-xhigh
    
    Porting-notes:
    
    - 2.3.x provides spa_config_enter_mmp(); use it where upstream uses spa_config_enter_priority()

--- module/zfs/zio.c
+++ module/zfs/zio.c
@@ -4445,7 +4445,7 @@ zio_vdev_io_start(zio_t *zio)
 			 * SCL_ZIO reader lock and may request it again here.
 			 * If there is another thread who wants the SCL_ZIO
 			 * writer lock, then scl_write_wanted will be set.
-			 * Thus, the spa_config_enter_priority() is used to
+			 * Thus, the spa_config_enter_mmp() is used to
 			 * ignore pending writer requests.
 			 *
 			 * The locking should be revised to remove the need
@@ -4455,7 +4455,7 @@ zio_vdev_io_start(zio_t *zio)
 			 * I/O balance while pruning.
 			 */
 			if (spa->spa_active_ddt_prune)
-				spa_config_enter_priority(spa, SCL_ZIO, zio,
+				spa_config_enter_mmp(spa, SCL_ZIO, zio,
 				    RW_READER);
 			else
 				spa_config_enter(spa, SCL_ZIO, zio,
