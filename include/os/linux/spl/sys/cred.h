2470eb370e8afd3e3e88b7e3113f1eccdfea7cec
Pavel Snajdr <snajpa@snajpa.net>
2026-04-06 18:57:33 +0000

    linux: complete ACL idmap plumbing
    Recent Linux ACL callback changes pass mount idmaps at the VFS boundary,
    but the ZPL still mixed init-userns assumptions into ACL conversion,
    callback selection, and chmod/create ACL setup.
    
    Add a shared idmap-to-userns helper, detect the idmap+dentry get_acl()
    variant, teach the ACL xattr conversion helpers to validate and encode
    against the caller's mount idmap, and thread zidmap_t through ACL
    initialization, chmod updates, and set_acl/get_acl dispatch.
    
    That finishes the ACL-specific side of the Linux idmap transition after
    the generic xattr write helpers already carry the mount idmap.
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- include/os/linux/spl/sys/cred.h
+++ include/os/linux/spl/sys/cred.h
@@ -74,21 +74,29 @@ static inline boolean_t zfs_no_idmapping(struct user_namespace *mnt_userns,
 	    mnt_userns == fs_userns);
 }
 
+static inline struct user_namespace *
+zfs_idmap_userns(zidmap_t *mnt_userns, struct user_namespace *tmp)
+{
+#ifdef HAVE_IOPS_CREATE_IDMAP
+	if (mnt_userns == zfs_init_idmap)
+		return (kcred->user_ns);
+#ifdef HAVE_IDMAP_NO_USERNS
+	tmp->uid_map = mnt_userns->uid_map;
+	tmp->gid_map = mnt_userns->gid_map;
+	return (tmp);
+#else
+	return (idmap_owner(mnt_userns));
+#endif
+#else
+	return (mnt_userns);
+#endif
+}
+
 static inline uid_t zfs_uid_to_vfsuid(zidmap_t *mnt_userns,
     struct user_namespace *fs_userns, uid_t uid)
 {
-	struct user_namespace *owner;
-#ifdef HAVE_IOPS_CREATE_IDMAP
-	if (mnt_userns == zfs_init_idmap)
-		return (uid);
-#endif
-#ifdef HAVE_IDMAP_NO_USERNS
 	struct user_namespace ns;
-	ns.uid_map = mnt_userns->uid_map;
-	owner = &ns;
-#else
-	owner = idmap_owner(mnt_userns);
-#endif
+	struct user_namespace *owner = zfs_idmap_userns(mnt_userns, &ns);
 	if (zfs_no_idmapping(owner, fs_userns))
 		return (uid);
 	if (!zfs_is_init_userns(fs_userns))
@@ -101,18 +109,8 @@ static inline uid_t zfs_uid_to_vfsuid(zidmap_t *mnt_userns,
 static inline gid_t zfs_gid_to_vfsgid(zidmap_t *mnt_userns,
     struct user_namespace *fs_userns, gid_t gid)
 {
-	struct user_namespace *owner;
-#ifdef HAVE_IOPS_CREATE_IDMAP
-	if (mnt_userns == zfs_init_idmap)
-		return (gid);
-#endif
-#ifdef HAVE_IDMAP_NO_USERNS
 	struct user_namespace ns;
-	ns.gid_map = mnt_userns->gid_map;
-	owner = &ns;
-#else
-	owner = idmap_owner(mnt_userns);
-#endif
+	struct user_namespace *owner = zfs_idmap_userns(mnt_userns, &ns);
 	if (zfs_no_idmapping(owner, fs_userns))
 		return (gid);
 	if (!zfs_is_init_userns(fs_userns))
@@ -125,18 +123,8 @@ static inline gid_t zfs_gid_to_vfsgid(zidmap_t *mnt_userns,
 static inline uid_t zfs_vfsuid_to_uid(zidmap_t *mnt_userns,
     struct user_namespace *fs_userns, uid_t uid)
 {
-	struct user_namespace *owner;
-#ifdef HAVE_IOPS_CREATE_IDMAP
-	if (mnt_userns == zfs_init_idmap)
-		return (uid);
-#endif
-#ifdef HAVE_IDMAP_NO_USERNS
 	struct user_namespace ns;
-	ns.uid_map = mnt_userns->uid_map;
-	owner = &ns;
-#else
-	owner = idmap_owner(mnt_userns);
-#endif
+	struct user_namespace *owner = zfs_idmap_userns(mnt_userns, &ns);
 	if (zfs_no_idmapping(owner, fs_userns))
 		return (uid);
 	uid = from_kuid(owner, KUIDT_INIT(uid));
@@ -150,18 +138,8 @@ static inline uid_t zfs_vfsuid_to_uid(zidmap_t *mnt_userns,
 static inline gid_t zfs_vfsgid_to_gid(zidmap_t *mnt_userns,
     struct user_namespace *fs_userns, gid_t gid)
 {
-	struct user_namespace *owner;
-#ifdef HAVE_IOPS_CREATE_IDMAP
-	if (mnt_userns == zfs_init_idmap)
-		return (gid);
-#endif
-#ifdef HAVE_IDMAP_NO_USERNS
 	struct user_namespace ns;
-	ns.gid_map = mnt_userns->gid_map;
-	owner = &ns;
-#else
-	owner = idmap_owner(mnt_userns);
-#endif
+	struct user_namespace *owner = zfs_idmap_userns(mnt_userns, &ns);
 	if (zfs_no_idmapping(owner, fs_userns))
 		return (gid);
 	gid = from_kgid(owner, KGIDT_INIT(gid));
