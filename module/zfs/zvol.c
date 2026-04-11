efb562b1935aaf2927a32fd00dd929903ff03d79
Pavel Snajdr <snajpa@snajpa.net>
2026-04-11 22:38:31 +0000

    zvol: reject encrypted cross-key block clones
    Mirror the regular file clone policy for encrypted datasets.
    zvol_clone_range() already rejects encrypted-vs-unencrypted clones, but
    it also needs to reject cross-objset encrypted clones whose objsets do
    not share the same master key.
    
    Without that check the zvol path can feed foreign-key ciphertext block
    pointers into dmu_brt_clone(), violating encrypted block-clone policy and
    correctness guarantees.
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- module/zfs/zvol.c
+++ module/zfs/zvol.c
@@ -632,6 +632,11 @@ zvol_clone_range(zvol_state_t *zv_src, uint64_t inoff, zvol_state_t *zv_dst,
 		error = SET_ERROR(EXDEV);
 		goto out;
 	}
+	if (inos != outos && inos->os_encrypted &&
+	    !dmu_objset_crypto_key_equal(inos, outos)) {
+		error = SET_ERROR(EXDEV);
+		goto out;
+	}
 	if (zv_src->zv_volblocksize != zv_dst->zv_volblocksize) {
 		error = SET_ERROR(EINVAL);
 		goto out;
