8d0f85e5ffebce397fcd3c67da88b99ddbc57e65
Pavel Snajdr <snajpa@snajpa.net>
2026-04-29 01:19:42 +0200

    zfs: clean up container statfs names

--- module/os/linux/zfs/zfs_vfsops.c
+++ module/os/linux/zfs/zfs_vfsops.c
@@ -150,8 +150,8 @@ static const match_table_t zpl_tokens = {
 	{ TOKEN_LAST,		NULL },
 };
 
-static uint_t zfs_statfs_shack_enabled = 0;
-ZFS_MODULE_PARAM(zfs, zfs_, statfs_shack_enabled, UINT, ZMOD_RW,
+static uint_t zfs_container_statfs_enabled = 0;
+ZFS_MODULE_PARAM(zfs, zfs_, container_statfs_enabled, UINT, ZMOD_RW,
 	"Enable per-process statfs f_type spoofing for container runtimes");
 
 vfs_t *
@@ -1207,7 +1207,7 @@ zfs_statvfs(struct inode *ip, struct kstatfs *statp)
 	zfsvfs_t *zfsvfs = ITOZSB(ip);
 	uint64_t refdbytes, availbytes, usedobjs, availobjs;
 	int err = 0;
-	bool magic_hack = false;
+	bool use_container_magic = false;
 
 	if ((err = zfs_enter(zfsvfs, FTAG)) != 0)
 		return (err);
@@ -1257,20 +1257,20 @@ zfs_statvfs(struct inode *ip, struct kstatfs *statp)
 	statp->f_namelen =
 	    zfsvfs->z_longname ? (ZAP_MAXNAMELEN_NEW - 1) : (MAXNAMELEN - 1);
 
-	if (zfs_statfs_shack_enabled != 0) {
+	if (zfs_container_statfs_enabled != 0) {
 		if (strcmp(current->comm, "containerd") == 0)
-			magic_hack = true;
+			use_container_magic = true;
 		if (strcmp(current->comm, "dockerd") == 0)
-			magic_hack = true;
+			use_container_magic = true;
 		if (strcmp(current->comm, "lxd") == 0)
-			magic_hack = true;
+			use_container_magic = true;
 		if (strcmp(current->comm, "podman") == 0)
-			magic_hack = true;
+			use_container_magic = true;
 		if (strcmp(current->comm, "crio") == 0)
-			magic_hack = true;
+			use_container_magic = true;
 	}
-	if (magic_hack)
-		statp->f_type = ZFS_SHACK_MAGIC;
+	if (use_container_magic)
+		statp->f_type = ZFS_CONTAINER_SUPER_MAGIC;
 
 	/*
 	 * We have all of 40 characters to stuff a string here.
