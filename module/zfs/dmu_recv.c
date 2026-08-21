8349594c282a87eb8c1450f0845a75d379c637ed
Richard Yao <richard@ryao.dev>
2026-08-21 12:52:08 -0400

    dmu_recv_begin_check: dsl_dataset_rele() should be called on ds
    Match each receive-begin plain dataset hold with a plain release. Error
    cleanup must not drop a decrypt/key mapping that this hold did not
    acquire.
    
    Upstream: acbcdd92cf4ddda9dde00c3d3e1c6515352e5d13
    Porting-notes: Applied the upstream source and its accompanying tests
     without adaptation to the selected 2.3.x fork.
    Reported-by: Grok 4.6 Build Beta
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Signed-off-by: Richard Yao <richard@ryao.dev>

--- module/zfs/dmu_recv.c
+++ module/zfs/dmu_recv.c
@@ -1034,8 +1034,7 @@ dmu_recv_begin_check(void *arg, dmu_tx_t *tx)
 				if (!redact_check(drba, origin)) {
 					dsl_dataset_rele_flags(origin, dsflags,
 					    FTAG);
-					dsl_dataset_rele_flags(ds, dsflags,
-					    FTAG);
+					dsl_dataset_rele(ds, FTAG);
 					return (SET_ERROR(EINVAL));
 				}
 			}
@@ -1043,7 +1042,7 @@ dmu_recv_begin_check(void *arg, dmu_tx_t *tx)
 			error = recv_check_large_blocks(ds, featureflags);
 			if (error != 0) {
 				dsl_dataset_rele_flags(origin, dsflags, FTAG);
-				dsl_dataset_rele_flags(ds, dsflags, FTAG);
+				dsl_dataset_rele(ds, FTAG);
 				return (error);
 			}
 
